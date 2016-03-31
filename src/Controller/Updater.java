53ded7470beaadc556f529187e830132eb101b79
xTuska <tuska95@gmail.com>
2016-03-30 22:19:44 -0400

    Added to updater, now updates view table to show inventory

--- src/Controller/Updater.java
+++ src/Controller/Updater.java
@@ -4,6 +4,7 @@ import Model.DealershipDB;
 import View.EditFrame;
 import View.MainFrame;
 import java.util.ArrayList;
+import Controller.Inventory;
 
 //exists to update the data on the tables of the GUI
 // will be run on the event listenter for when a window is set to visible
@@ -17,8 +18,8 @@ public class Updater
         switch (selected) {
             case "Employees":
                 ef.updateTitle("Employee");
-                String[] columns = {"Name","Phone #","Email","Department","Salary"};
-                ef.updateColumnNames(columns);
+                String[] columns1 = {"Name","Phone #","Email","Department","Salary"};
+                ef.updateColumnNames(columns1);
                 //cant test if this works without database setup and existing
                 /*
                 ArrayList<Employees> emp = db.getEmployees();
@@ -32,7 +33,21 @@ public class Updater
                     ef.addRow(info);
                 }*/
                 break;
-            case "Invetory":
+            case "Inventory":
+                ef.updateTitle("Inventory");
+                String[] columns2 = {"Model","Make","Stock","Price","Code"};
+                ef.updateColumnNames(columns2);
+                /*
+                ArrayList<Inventory> inv = db.getInventory();
+                for(Inventory x : inv)
+                {
+                    String[] info = {x.getModel(),
+                                     x.getMake(),
+                                     Double.toString(x.getStock()),
+                                     Double.toString(x.getPrice()),
+                                     x.getCode()};
+                    ef.addRow(info);
+                }*/
                 break;
             case "Sales":
                 break;
