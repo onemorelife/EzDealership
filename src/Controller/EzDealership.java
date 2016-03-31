cf72a17f07fcf5ddfa5285327b75af4b8d77e384
onemorelife <bkhan9279@gmail.com>
2016-03-30 21:32:49 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	nbproject/private/private.properties

--- src/Controller/EzDealership.java
+++ src/Controller/EzDealership.java
@@ -6,7 +6,10 @@
 
 package Controller;
 
+import View.EditFrame;
 import View.MainFrame;
+import View.SearchFrame;
+import java.util.ArrayList;
 
 /**
  *
@@ -17,7 +20,12 @@ public class EzDealership {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String args[]) 
+    {
+        ArrayList<String> tableNames = new ArrayList<>();
+        tableNames.add("Employees");
+        tableNames.add("Inventory");
+        tableNames.add("Sales");
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -44,8 +52,10 @@ public class EzDealership {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                MainFrame mf = new MainFrame();
-                mf.setVisible(true);
+                EditFrame ef = new EditFrame();
+                SearchFrame sf = new SearchFrame();
+                MainFrame mf = new MainFrame(ef, sf);
+                mf.updateCbxTableList(tableNames);
             }
         });
     }
