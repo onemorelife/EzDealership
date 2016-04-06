a1cdfb5a12c1326c5394e395922509cd28ba0000
onemorelife <bkhan9279@gmail.com>
2016-04-05 20:14:34 -0400

    Fixed dealership script

---
+++ src/Model/DealershipDBCreate.bat
@@ -0,0 +1,8 @@
+:: Change to the directory that stores the script
+cd \ezDealership\src\Model
+
+:: Use the ij tool to run the script
+java org.apache.derby.tools.ij DealershipDBCreate.sql
+
+:: Keep the command prompt open until the user presses a key
+pause
--- src/Model/DealershipDBCreate.sql
+++ src/Model/DealershipDBCreate.sql
@@ -8,7 +8,7 @@ DROP TABLE Employees;
 -- create Inventory table
 CREATE TABLE Inventory
 (
-        make VARCHAR(20), 
+    make VARCHAR(20), 
 	model VARCHAR(20), 
 	stock INT,
 	price DOUBLE,
@@ -25,8 +25,8 @@ INSERT INTO Inventory VALUES
 INSERT INTO Inventory VALUES
 ('Nissan', 'Sentra', 8, 19800, 'NISS'); 
 
--- view data in Products table
-SELECT * FROM Products;
+-- view data in Inventory table
+SELECT * FROM Inventory;
 
 -- create Employees table 
 CREATE TABLE Employees 
@@ -36,37 +36,37 @@ CREATE TABLE Employees
 	email VARCHAR(20),
 	department VARCHAR(20),
 	salary DOUBLE
-)
+);
 
 --insert data into Employees table 
 INSERT INTO Employees VALUES 
-('John Doe', '4332125678', 'jdoe@example.com', 'Sales', 32000)
+('John Doe', '4332125678', 'jdoe@example.com', 'Sales', 32000);
 
 INSERT INTO Employees VALUES 
-('Jane Doe', '3224785678', 'janedoe@example.com', 'Accounting', 32000)
+('Jane Doe', '3224785678', 'janedoe@example.com', 'Accounting', 32000);
 
 INSERT INTO Employees VALUES 
-('Bob Smith', '6167894342', 'bsmith@example.com', 'Sales', 38000)
+('Bob Smith', '6167894342', 'bsmith@example.com', 'Sales', 38000);
 
 -- view data in Employees table
 SELECT * FROM Employees;
 
--- create Employees table 
-CREATE TABLE Sales 
+-- create Sales table 
+CREATE TABLE Sales
 (
 	code VARCHAR(4),
 	quantity INT,
 	employeeName VARCHAR(24),
 	salesDate VARCHAR(8),
 	lease BOOLEAN
-)
+);
 
 --insert data into Sales table
 INSERT INTO Sales VALUES
-('TSLX', 1, 'Bob Smith', '12182015', 1)
+('TSLX', 1, 'Bob Smith', '12182015', FALSE);
 
 INSERT INTO Sales VALUES 
-('NISS', 1, 'John Doe', '03152016', 1)
+('NISS', 1, 'John Doe', '03152016', FALSE);
 
 --View data in Sales table 
 SELECT * FROM Sales; 
